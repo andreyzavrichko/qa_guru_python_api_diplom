124825969014986016d99b78cf3781ddb7a5c914
Andrey <andrey@zavrichko.dev>
2024-08-28 19:24:55 +0300

    start

--- api/requests.py
+++ api/requests.py
@@ -1,53 +1,7 @@
-import logging
-import json
 import allure
-from curlify import to_curl
-from requests import Response
-from allure_commons.types import AttachmentType
 import requests
 
-
-def response_logging(response: Response):
-    logging.info("Request: " + response.request.url)
-
-    if response.request.body:
-        # Decode the request body from bytes to a string
-        request_body = response.request.body.decode('utf-8')
-        logging.info("INFO Request body: " + request_body)  # Log the decoded request body
-
-    logging.info("Request headers: " + str(response.request.headers))
-    logging.info("Response code: " + str(response.status_code))
-    logging.info("Response: " + response.text)
-
-
-def request_attaching(response: Response):
-    with allure.step(f'{response.request.method} {response.request.url}'):
-        curl = to_curl(response.request)
-        logging.info(curl)
-        logging.info(f'status code: {response.status_code}')
-        allure.attach(body=curl, name='curl', attachment_type=allure.attachment_type.TEXT, extension='txt')
-
-
-def response_attaching(response: Response):
-    allure.attach(
-        body=response.request.url,
-        name="Request url",
-        attachment_type=AttachmentType.TEXT,
-    )
-
-    if response.request.body:  # логирование тела запроса если оно есть
-        allure.attach(
-            body=response.request.body.decode('utf-8') if response.request.body else None,
-            name="Request body",
-            attachment_type=AttachmentType.JSON,
-            extension="json",
-        )
-        allure.attach(
-            body=json.dumps(response.json(), indent=4, ensure_ascii=True),
-            name="Response",
-            attachment_type=AttachmentType.JSON,
-            extension="json",
-        )
+from utils.attach import response_logging, response_attaching, request_attaching
 
 
 def get_request(url, params=None):
--- utils/attach.py
+++ utils/attach.py
@@ -1,30 +1,49 @@
 import json
+import logging
 
 import allure
+from allure_commons.types import AttachmentType
+from curlify import to_curl
+from requests import Response
 
 
-def request_body(request_body_json):
+def response_logging(response: Response):
+    logging.info("Request: " + response.request.url)
+
+    if response.request.body:
+        request_body = response.request.body.decode('utf-8')
+        logging.info("INFO Request body: " + request_body)
+
+    logging.info("Request headers: " + str(response.request.headers))
+    logging.info("Response code: " + str(response.status_code))
+    logging.info("Response: " + response.text)
+
+
+def request_attaching(response: Response):
+    with allure.step(f'{response.request.method} {response.request.url}'):
+        curl = to_curl(response.request)
+        logging.info(curl)
+        logging.info(f'status code: {response.status_code}')
+        allure.attach(body=curl, name='curl', attachment_type=allure.attachment_type.TEXT, extension='txt')
+
+
+def response_attaching(response: Response):
     allure.attach(
-        name='Request body',
-        body=request_body_json,
-        attachment_type=allure.attachment_type.JSON,
-        extension='json'
+        body=response.request.url,
+        name="Request url",
+        attachment_type=AttachmentType.TEXT,
     )
 
-
-def response_body(response_body_json):
-    allure.attach(
-        name='Response body',
-        body=json.dumps(response_body_json),
-        attachment_type=allure.attachment_type.JSON,
-        extension='json'
-    )
-
-
-def response_code(response_status_code):
-    allure.attach(
-        name='Response status code',
-        body=response_status_code,
-        attachment_type=allure.attachment_type.TEXT,
-        extension='txt'
-    )
+    if response.request.body:
+        allure.attach(
+            body=response.request.body.decode('utf-8') if response.request.body else None,
+            name="Request body",
+            attachment_type=AttachmentType.JSON,
+            extension="json",
+        )
+        allure.attach(
+            body=json.dumps(response.json(), indent=4, ensure_ascii=True),
+            name="Response",
+            attachment_type=AttachmentType.JSON,
+            extension="json",
+        )
